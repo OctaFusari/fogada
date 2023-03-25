6e2bf6ee9491093f8378aa5632c3951f809d9ea0
octavian.fusari <octavian.fusari@marconirovereto.it>
2023-03-25 01:38:29 +0100

    mods

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -1,4 +1,5 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, ElementRef, OnInit, ViewChild } from '@angular/core';
+import * as THREE from "three";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,46 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  @ViewChild('animazioneCentro', { static: true })
+  canvasElementRef!: ElementRef<HTMLCanvasElement>;
+  
   constructor() { }
 
   ngOnInit(): void {
+    const scene= new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+
+    const renderer = new THREE.WebGLRenderer();
+    renderer.setSize( window.innerWidth, window.innerHeight );
+    document.body.appendChild( renderer.domElement );
+    renderer.setClearColor(0x0E0E0E, 0);
+    
+    document.body.appendChild( renderer.domElement );
+
+    const curve = new THREE.SplineCurve( [
+      new THREE.Vector2( 10, 0 ),
+      new THREE.Vector2( 20, 20 ),
+      new THREE.Vector2( 30, 20 ),
+    ] );
+    
+    const points = curve.getPoints( 1000 );
+    const geometry = new THREE.BufferGeometry().setFromPoints( points );
+    
+    const material = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.DoubleSide} );
+    
+    
+    // Create the final object to add to the scene
+    const splineObject = new THREE.Line( geometry, material );
+
+    scene.add( splineObject );
+
+    camera.position.z = 40;
+
+    function animate() {
+      requestAnimationFrame( animate );
+      renderer.render( scene, camera );
+    }
+    /* animate(); */
   }
 
   octimal(){
