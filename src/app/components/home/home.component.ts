bd3c73c77f85a103170f72d1abf95a5fb3c7546b
octavian.fusari <octavian.fusari@marconirovereto.it>
2023-03-27 00:38:38 +0200

    mods

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -24,30 +24,83 @@ export class HomeComponent implements OnInit {
     
     document.body.appendChild( renderer.domElement );
 
-    const curve = new THREE.SplineCurve( [
-      new THREE.Vector2( 10, 0 ),
-      new THREE.Vector2( 20, 20 ),
-      new THREE.Vector2( 30, 20 ),
+    
+    let curve__x = 0;
+    let curve__y = 0;
+
+    if(curve__x <= 1){
+      curve__x += 0.1
+    }
+
+    const curve = new THREE.CatmullRomCurve3( [
+      new THREE.Vector3( curve__x-70, curve__y-35 ),
+      new THREE.Vector3( curve__x-60, curve__y-25 ),
+      new THREE.Vector3( curve__x-30, curve__y-10 ),
+      new THREE.Vector3( curve__x-20, curve__y-8 ),
+      new THREE.Vector3( curve__x-10, curve__y ),
+      new THREE.Vector3( curve__x+10, curve__y+5 ),
+      new THREE.Vector3( 30, 7 ),
+      
+      new THREE.Vector3( curve__x+32, curve__y+15 ),
+      new THREE.Vector3( curve__x+30, curve__y+15 ),
+      new THREE.Vector3( curve__x+30, curve__y+17 ),
+      new THREE.Vector3( curve__x+32, curve__y+17 ),
+      new THREE.Vector3( curve__x+32, curve__y+19 ),
+      new THREE.Vector3( curve__x+34, curve__y+19 ),
+      new THREE.Vector3( curve__x+34, curve__y+17 ),
+      new THREE.Vector3( curve__x+37, curve__y+17 ),
+      new THREE.Vector3( curve__x+37, curve__y+15 ),
+      new THREE.Vector3( curve__x+34, curve__y+15 ),
+      new THREE.Vector3( curve__x+35, curve__y+7 ),
+
+      new THREE.Vector3( curve__x+40, curve__y+6 ),
+      new THREE.Vector3( curve__x+50, curve__y+4 ),
+      new THREE.Vector3( curve__x+60, curve__y+0 ),
+      new THREE.Vector3( curve__x+80, curve__y-5 ),
     ] );
     
     const points = curve.getPoints( 1000 );
     const geometry = new THREE.BufferGeometry().setFromPoints( points );
     
-    const material = new THREE.MeshBasicMaterial( { color: 0xff0000, side: THREE.DoubleSide} );
-    
+    const material = new THREE.LineBasicMaterial( {
+      color: 0x006688,
+      
+    } );
     
     // Create the final object to add to the scene
-    const splineObject = new THREE.Line( geometry, material );
+    const splineObject = new THREE.LineSegments( geometry, material );
 
     scene.add( splineObject );
 
     camera.position.z = 40;
 
+    const grandezze = {
+      width: window.innerWidth,
+      height: window.innerHeight
+    }
+
+    
+
+    window.addEventListener('resize', (size: any) => {
+      grandezze.width = window.innerWidth;
+      grandezze.height = window.innerHeight;
+
+      camera.aspect = grandezze.width / grandezze.height;
+      camera.updateProjectionMatrix();
+
+      renderer.setSize(grandezze.width, grandezze.height);
+      renderer.setPixelRatio(Math.min(window.devicePixelRatio, 2))
+
+      renderer.render(scene, camera); // -> Also needed
+      console.log(window.innerHeight / 160)
+
+    })
+
     function animate() {
       requestAnimationFrame( animate );
       renderer.render( scene, camera );
     }
-    /* animate(); */
+    animate();
   }
 
   octimal(){
