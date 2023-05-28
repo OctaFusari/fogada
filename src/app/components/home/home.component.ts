16bce91ce4168ce754697a29a6a01faa2cf09c15
octavian.fusari <octavian.fusari@marconirovereto.it>
2023-05-29 00:20:39 +0200

    mods

--- src/app/components/home/home.component.ts
+++ src/app/components/home/home.component.ts
@@ -12,80 +12,80 @@ export class HomeComponent implements OnInit {
   canvasElementRef!: ElementRef<HTMLCanvasElement>;
 
   coreBody = 0;
-  changeState(mode:any){
-    if (mode == "sci"){
+  changeState(mode: any) {
+    if (mode == "sci") {
       this.coreBody = 1
       document.documentElement.style.setProperty('--background', 'rgba(14, 14, 14)');
       document.documentElement.style.setProperty('--text', 'rgb(201, 201, 201)');
       document.documentElement.style.setProperty('--text__back', 'rgba(36, 36, 36, 0.267)');
-    }else{
+    } else {
       this.coreBody = 0
       document.documentElement.style.setProperty('--background', 'rgba(255, 255, 255)');
       document.documentElement.style.setProperty('--text', 'rgba(63, 63, 63)');
       document.documentElement.style.setProperty('--text__back', 'rgba(221, 221, 221, 0.267)');
     }
   }
-  
+
   constructor() { }
 
   ngOnInit(): void {
-    const scene= new THREE.Scene();
-    const camera = new THREE.PerspectiveCamera( 75, window.innerWidth / window.innerHeight, 0.1, 1000 );
+    const scene = new THREE.Scene();
+    const camera = new THREE.PerspectiveCamera(75, window.innerWidth / window.innerHeight, 0.1, 1000);
 
     const renderer = new THREE.WebGLRenderer();
-    renderer.setSize( window.innerWidth, window.innerHeight );
-    document.body.appendChild( renderer.domElement );
+    renderer.setSize(window.innerWidth, window.innerHeight);
+    document.body.appendChild(renderer.domElement);
     renderer.setClearColor(0x0E0E0E, 0);
-    
-    document.body.appendChild( renderer.domElement );
 
-    
+    document.body.appendChild(renderer.domElement);
+
+
     let curve__x = 0;
     let curve__y = 0;
 
-    if(curve__x <= 1){
+    if (curve__x <= 1) {
       curve__x += 0.1
     }
 
-    let curve = new THREE.CatmullRomCurve3( [
-      new THREE.Vector3( curve__x-70, curve__y-25 ),
-      new THREE.Vector3( curve__x-60, curve__y-23 ),
-      new THREE.Vector3( curve__x-30, curve__y-14 ),
-      new THREE.Vector3( curve__x-20, curve__y-10 ),
-      new THREE.Vector3( curve__x-10, curve__y-9 ),
-      new THREE.Vector3( curve__x+14, curve__y+6 ),
-      new THREE.Vector3( curve__x+34, curve__y+12 ),
-      
-      new THREE.Vector3( curve__x+36, curve__y+19 ),
-      new THREE.Vector3( curve__x+34, curve__y+19 ),
-      new THREE.Vector3( curve__x+34, curve__y+21 ),
-      new THREE.Vector3( curve__x+36, curve__y+21 ),
-      new THREE.Vector3( curve__x+36, curve__y+23 ),
-      new THREE.Vector3( curve__x+38, curve__y+23 ),
-      new THREE.Vector3( curve__x+38, curve__y+21 ),
-      new THREE.Vector3( curve__x+41, curve__y+21 ),
-      new THREE.Vector3( curve__x+41, curve__y+19 ),
-      new THREE.Vector3( curve__x+38, curve__y+19 ),
-      new THREE.Vector3( curve__x+39, curve__y+12 ),
+    let curve = new THREE.CatmullRomCurve3([
+      new THREE.Vector3(curve__x - 70, curve__y - 25),
+      new THREE.Vector3(curve__x - 60, curve__y - 23),
+      new THREE.Vector3(curve__x - 30, curve__y - 14),
+      new THREE.Vector3(curve__x - 20, curve__y - 10),
+      new THREE.Vector3(curve__x - 10, curve__y - 9),
+      new THREE.Vector3(curve__x + 14, curve__y + 6),
+      new THREE.Vector3(curve__x + 34, curve__y + 12),
 
-      new THREE.Vector3( curve__x+44, curve__y+10 ),
-      new THREE.Vector3( curve__x+50, curve__y+8 ),
-      new THREE.Vector3( curve__x+70, curve__y+2 ),
-      new THREE.Vector3( curve__x+80, curve__y-8 ),
-    ] );
-    
-    const points = curve.getPoints( 1000 );
-    const geometry = new THREE.BufferGeometry().setFromPoints( points );
-    
-    const material = new THREE.LineBasicMaterial( {
+      new THREE.Vector3(curve__x + 36, curve__y + 19),
+      new THREE.Vector3(curve__x + 34, curve__y + 19),
+      new THREE.Vector3(curve__x + 34, curve__y + 21),
+      new THREE.Vector3(curve__x + 36, curve__y + 21),
+      new THREE.Vector3(curve__x + 36, curve__y + 23),
+      new THREE.Vector3(curve__x + 38, curve__y + 23),
+      new THREE.Vector3(curve__x + 38, curve__y + 21),
+      new THREE.Vector3(curve__x + 41, curve__y + 21),
+      new THREE.Vector3(curve__x + 41, curve__y + 19),
+      new THREE.Vector3(curve__x + 38, curve__y + 19),
+      new THREE.Vector3(curve__x + 39, curve__y + 12),
+
+      new THREE.Vector3(curve__x + 44, curve__y + 10),
+      new THREE.Vector3(curve__x + 50, curve__y + 8),
+      new THREE.Vector3(curve__x + 70, curve__y + 2),
+      new THREE.Vector3(curve__x + 80, curve__y - 8),
+    ]);
+
+    const points = curve.getPoints(1000);
+    const geometry = new THREE.BufferGeometry().setFromPoints(points);
+
+    const material = new THREE.LineBasicMaterial({
       color: 0x006688,
-      
-    } );
-    
-    // Create the final object to add to the scene
-    const splineObject = new THREE.LineSegments( geometry, material );
 
-    scene.add( splineObject );
+    });
+
+    // Create the final object to add to the scene
+    const splineObject = new THREE.LineSegments(geometry, material);
+
+    scene.add(splineObject);
 
     camera.position.z = 40;
 
@@ -111,26 +111,26 @@ export class HomeComponent implements OnInit {
 
     function animate() {
 
-      if (window.innerWidth < 1111){
+      if (window.innerWidth < 1111) {
         camera.position.x = 30;
         camera.position.y = 8;
         camera.position.z = 25;
-      }else{
+      } else {
         camera.position.x = 0;
         camera.position.y = 0;
         camera.position.z = 40;
       }
 
-      requestAnimationFrame( animate );
-      renderer.render( scene, camera );
+      requestAnimationFrame(animate);
+      renderer.render(scene, camera);
     }
     animate();
 
-    var carousel:any = document.querySelector('.body__galleria');
-    var carousel__reverse:any = document.querySelector('.body__galleria__reverse');
-    var galleria__plus:any = document.querySelector('.galleria__plus') as HTMLElement;
+    var carousel: any = document.querySelector('.body__galleria');
+    var carousel__reverse: any = document.querySelector('.body__galleria__reverse');
+    var galleria__plus: any = document.querySelector('.galleria__plus') as HTMLElement;
 
-    const textElement:any = document.querySelector('.text');
+    const textElement: any = document.querySelector('.text');
     const observer = new IntersectionObserver((entries) => {
       entries.forEach((entry) => {
         if (entry.isIntersecting) {
@@ -138,9 +138,9 @@ export class HomeComponent implements OnInit {
         }
       });
     });
-    
+
     observer.observe(textElement);
-    
+
     function animateText() {
       let count = 0;
       const animationDuration = 1000;
@@ -149,38 +149,58 @@ export class HomeComponent implements OnInit {
       const intervalId = setInterval(() => {
         count += increment;
         textElement.textContent = count.toString();
-       
+
         if (count >= targetValue) {
           clearInterval(intervalId);
         }
       }, animationDuration / (targetValue / increment));
     }
-    
+
     let lastscroll = 0;
 
     carousel.scrollLeft += 600;
     carousel__reverse.scrollLeft += 3000;
 
-    window.addEventListener('scroll', (size:any) => {
-      console.log(scrollY)
-      if(lastscroll < scrollY){
+    window.addEventListener('scroll', (size: any) => {
+
+      const scrollPercentage = (window.scrollY / (document.documentElement.scrollHeight - window.innerHeight)) * 100;
+      const line = document.querySelector('.line') as HTMLElement;
+      /*       line.style.width = `${scrollPercentage}%`;
+       */
+      var h: any = document.documentElement,
+        b: any = document.body,
+        st: any = 'scrollTop',
+        sh: any = 'scrollHeight';
+
+      var percent = (h[st] || b[st]) / ((h[sh] || b[sh]) - h.clientHeight) * 100;
+      if (window.innerWidth > 1200) {
+        line.style.width = percent * 16.5 + "px"
+      } else {
+        line.style.width = percent * 16.5 + "px"
+      }
+
+      if (lastscroll < scrollY) {
         carousel.scrollLeft += 2;
         carousel__reverse.scrollLeft -= 2;
-      }else if(lastscroll > scrollY){
+      } else if (lastscroll > scrollY) {
         carousel.scrollLeft -= 2;
         carousel__reverse.scrollLeft += 2;
       }
       lastscroll = scrollY
+
+
+      
+
     })
 
     let targetTime = new Date("2023-06-10T00:00:00").getTime();
     this.countdown(targetTime);
   }
-  
+
 
 
   countdown(targetTime: number) {
-    const outputElement:any = document.getElementById("countdown-output");
+    const outputElement: any = document.getElementById("countdown-output");
     const interval = setInterval(() => {
       const now = new Date().getTime();
       const remainingTime = Math.floor((targetTime - now) / 1000);
@@ -193,18 +213,18 @@ export class HomeComponent implements OnInit {
         const seconds = Math.floor(remainingTime % 60);
         let date = `${days} giorni, ${hours} ore, ${minutes} minuti, ${seconds} alla gara`
         outputElement.innerText = date.toString();
-        
+
       }
     }, 1000);
   }
 
-  octimal(){
+  octimal() {
     window.location.href = "https://octimal.it/";
   }
 
 
-  form(){
-    window.location.href ="https://docs.google.com/forms/d/1-If1BUC846O_ovDsTYCNOjgesHE7IYLZVVn_WnIIOnU/viewform?edit_requested=true";
+  form() {
+    window.location.href = "https://docs.google.com/forms/d/1-If1BUC846O_ovDsTYCNOjgesHE7IYLZVVn_WnIIOnU/viewform?edit_requested=true";
   }
 
   scroll(el: HTMLElement) {
